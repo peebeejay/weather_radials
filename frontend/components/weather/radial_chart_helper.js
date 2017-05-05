0d2add490d575fd5d30d51a7ae7afc543b74b5f0
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2017-05-04 21:59:11 -0400

    add tooltips

--- frontend/components/weather/radial_chart_helper.js
+++ frontend/components/weather/radial_chart_helper.js
@@ -22,6 +22,10 @@ export const generateSpokes = (barWrapper, innerRadius, outerRadius) => {
 
 
 export const plotBars = (_weather, barWrapper, angle, barScale, colorScale) => {
+  let div = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
   let tempDifferences = [];
 
   for (let x = 0; x < _weather.list.length - 1; x++) {
@@ -32,17 +36,28 @@ export const plotBars = (_weather, barWrapper, angle, barScale, colorScale) => {
   barWrapper.selectAll(".tempBar")
     .data(tempDifferences)
     .enter().append("rect")
-    .attr("class", "tempBar")
-    .attr("transform", function(d,i) { return "rotate(" + (angle(d.dt)) + ")"; })
-    .attr("width", 9)
-    .attr("height", function(d,i) {
-      // debugger
-      return barScale(d.main.temp_max) - barScale(d.main.temp_min); })
+    // .attr("class", "tempBar")
+    .attr("class", (d, i) => `tempBar tempBar-${i}`)
+    .attr("transform", (d,i) => { return "rotate(" + (angle(d.dt)) + ")"; })
+    .attr("width", 10)
+    .attr("height", (d,i) => { return barScale(d.main.temp_max) - barScale(d.main.temp_min); })
     .attr("x", -0.75)
-    .attr("y", function(d,i) {return barScale(d.main.temp_min); })
-    .style("fill", function(d) {
-      // debugger
-      return colorScale((d.main.temp_max + d.main.temp_min) / 2.0); });
+    .attr("y", (d,i) => {return barScale(d.main.temp_min); })
+    .attr("tempMin", (d) => `${d.main.temp_min}`)
+    .attr("tempMax", (d) => `${d.main.temp_max}`)
+    .attr("date", (d) => `${ d3.timeFormat("%H:%M")(new Date(d.dt * 1000)) }`)
+    .style("fill", (d) => { return colorScale((d.main.temp_max + d.main.temp_min) / 2.0); })
+    .on("mouseover", (d) => {
+       div.transition().duration(50).style("opacity", .9);
+       div.html(d3.timeFormat("%B %d, %Y %H:%M")(new Date(d.dt * 1000)) + "<br/> " + d.main.temp_min + "°F - " + d.main.temp_max + "°F")
+         .style("left", (d3.event.pageX) + "px")
+         .style("top", (d3.event.pageY - 28) + "px");
+       })
+    .on("mouseout", (d) => {
+       div.transition()
+         .duration(500)
+         .style("opacity", 0);
+       });
 };
 
 export const plotDates = (_weather, outerRadius, barWrapper) => {
