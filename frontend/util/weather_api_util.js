c22a470eedb2bfe55abc2392e3dd505e130d8bba
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2017-05-07 23:28:20 -0400

    https for weather

--- frontend/util/weather_api_util.js
+++ frontend/util/weather_api_util.js
@@ -1,14 +1,14 @@
 export const fetch5Day = (lat, lon) => {
   return $.ajax({
     method: "GET",
-    url: `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&APPID=bcb83c4b54aee8418983c2aff3073b3b&mode=json&units=imperial`
+    url: `https://crossorigin.me/http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&APPID=bcb83c4b54aee8418983c2aff3073b3b&mode=json&units=imperial`
   });
 };
 
 export const fetchDaily = (lat, lon) => {
   return $.ajax({
     method: "GET",
-    url: `http://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&APPID=bcb83c4b54aee8418983c2aff3073b3b&mode=json&units=imperial`
+    url: `https://crossorigin.me/http://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&APPID=bcb83c4b54aee8418983c2aff3073b3b&mode=json&units=imperial`
   });
 };
 
